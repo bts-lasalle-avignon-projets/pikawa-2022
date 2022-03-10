d5d91b62251b952a9c73ee1a72e322975a3b2e25
abryckaert <bryckaert.anthony@gmail.com>
2022-03-10 09:33:59 +0100

    Ajout squelette classe preparation

---
+++ pikawa-qt/preparation.h
@@ -0,0 +1,18 @@
+#ifndef PREPARATION_H
+#define PREPARATION_H
+#include <vector>
+#include <string.h>
+
+class preparation
+{    
+private:
+    vector<std::string> nomsCapsules;
+    vector<std::string> nomsBoisson;
+    bool capsulePresente;
+    bool tassePresente;
+
+public:
+    preparation();
+};
+
+#endif // PREPARATION_H
