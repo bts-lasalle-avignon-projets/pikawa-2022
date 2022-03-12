f8b8e3b30213c7b289ff91174f0498fb585e6697
tvaira <tvaira@free.fr>
2022-03-12 12:12:09 +0100

    PIKAWA22-11 Modifie la structure de l'IHM

--- pikawa-qt/ihm.cpp
+++ pikawa-qt/ihm.cpp
@@ -1,15 +1,32 @@
 #include "ihm.h"
 #include "ui_ihm.h"
+#include <QDebug>
 
-ihm::ihm(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::ihm)
+/**
+ * @file ihm.cpp
+ *
+ * @brief Définition de la classe IHMPikawa
+ * @author
+ * @version 1.0
+ *
+ */
+
+IHMPikawa::IHMPikawa(QWidget* parent) :
+    QMainWindow(parent), ui(new Ui::IHMPikawa)
 {
     ui->setupUi(this);
+    qDebug() << Q_FUNC_INFO;
+
+    ui->statusbar->showMessage(QString::fromUtf8("Pikawa 2022"));
+
+#ifdef PLEIN_ECRAN
+    showFullScreen();
+// showMaximized();
+#endif
 }
 
-ihm::~ihm()
+IHMPikawa::~IHMPikawa()
 {
     delete ui;
+    qDebug() << Q_FUNC_INFO;
 }
-
--- pikawa-qt/ihm.h
+++ pikawa-qt/ihm.h
@@ -1,21 +1,45 @@
 #ifndef IHM_H
 #define IHM_H
 
-#include <QMainWindow>
+/**
+ * @file ihm.h
+ *
+ * @brief Déclaration de la classe IHMPikawa
+ * @author
+ * @version 1.0
+ *
+ */
+
+#include <QtWidgets>
+
+/**
+ * @def PLEIN_ECRAN
+ * @brief Pour le mode plein écran
+ */
+//#define PLEIN_ECRAN
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class ihm; }
+namespace Ui
+{
+class IHMPikawa;
+}
 QT_END_NAMESPACE
 
-class ihm : public QMainWindow
+/**
+ * @class IHMPikawa
+ * @brief Déclaration de la classe IHMPikawa
+ * @details Cette classe s'occupe de l'affichage sur l'écran de la Raspeberry Pi
+ */
+class IHMPikawa : public QMainWindow
 {
     Q_OBJECT
 
-public:
-    ihm(QWidget *parent = nullptr);
-    ~ihm();
+  public:
+    IHMPikawa(QWidget* parent = nullptr);
+    ~IHMPikawa();
 
-private:
-    Ui::ihm *ui;
+  private:
+    Ui::IHMPikawa* ui; //!< la fenêtre graphique associée à cette classe
 };
+
 #endif // IHM_H
