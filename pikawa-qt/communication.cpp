8739ec873897e972d38cb87dcf96f8f57fb22a85
abryckaert <bryckaert.anthony@gmail.com>
2022-04-01 22:38:38 +0200

    PIKAWA22-34 Ajout du triage des appareils Bluetooth et conservation de la machine

--- pikawa-qt/communication.cpp
+++ pikawa-qt/communication.cpp
@@ -63,10 +63,16 @@ void Communication::activerLaDecouverte()
 void Communication::decouvrirCafetiere(
   const QBluetoothDeviceInfo& appareilBluetooth)
 {
-    qDebug() << Q_FUNC_INFO << "appareil Bluetooth" << appareilBluetooth.name()
+    if(appareilBluetooth.name().contains("PIKAWA"))
+    {
+        qDebug() << Q_FUNC_INFO << "machine à café détectée" << appareilBluetooth.name()
+                 << '[' << appareilBluetooth.address().toString() << ']';
+
+        QBluetoothDeviceInfo pikawa = appareilBluetooth;
+    }
+    else
+    {
+        qDebug() << Q_FUNC_INFO << "appareil Bluetooth" << appareilBluetooth.name()
              << '[' << appareilBluetooth.address().toString() << ']';
-    /**
-     * @todo Vérifier que le nom de l'appareil Bluetooth contient l'identifiant
-     * PIKAWA, si c'est le cas conserver cet appareil
-     */
+    }
 }
