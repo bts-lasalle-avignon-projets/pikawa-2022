27c19f515cd9acfdb76465883a298a87de6a9be8
abryckaert <bryckaert.anthony@gmail.com>
2022-04-01 08:06:58 +0200

    PIKAWA22-28 Correction des erreurs

--- pikawa-qt/cafetiere.cpp
+++ pikawa-qt/cafetiere.cpp
@@ -14,10 +14,10 @@
  */
 
 Cafetiere::Cafetiere(IHMPikawa* ihm) :
-    QObject(ihm), ihm(ihm), communication(communication), preparation(preparation),
-    nomCapsules(0), nomBoissons(0), capsuleChoisie(0), boissonChoisie(0),
-    niveauEau(0), connectee(false), activee(false), capsulePresente(false),
-    tassePresente(false)
+    QObject(ihm), ihm(ihm), communication(new Communication(this)),
+    preparation(new Preparation(this)), nomCapsules(0), nomBoissons(0),
+    capsuleChoisie(0), boissonChoisie(0), niveauEau(0), connectee(false),
+    activee(false), capsulePresente(false), tassePresente(false)
 {
     qDebug() << Q_FUNC_INFO;
 }
