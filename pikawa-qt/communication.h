55fda9138321b7a3d012e78f156ab98fa766ee06
tvaira <tvaira@free.fr>
2022-04-03 10:37:43 +0200

    PIKAWA22-34 Fixe les attributs correctement

--- pikawa-qt/communication.h
+++ pikawa-qt/communication.h
@@ -11,8 +11,10 @@
  */
 
 #include <QObject>
-#include <QBluetoothDeviceInfo>
-#include <QBluetoothLocalDevice>
+#include <QtBluetooth>
+//#include <QBluetoothDeviceInfo>
+//#include <QBluetoothLocalDevice>
+//#include <QBluetoothDeviceDiscoveryAgent>
 
 /**
  * @def PREFIXE_NOM_CAFETIERE
@@ -30,23 +32,32 @@ class Communication : public QObject
 {
     Q_OBJECT
   private:
-    QBluetoothLocalDevice interfaceLocale;
-    bool                  active;
-    bool                  connecte;
+    QBluetoothLocalDevice interfaceLocale; //!< l'interface Bluetooth locale
+    QBluetoothDeviceDiscoveryAgent*
+      agentDecouvreur; //!< agent permettant de découvrir des interfaces
+                       //!< Bluetooth
+    QBluetoothDeviceInfo
+         pikawa;        //!< l'interface Bluetooth de la cafetière pikawa
+    bool pikawaDetecte; //!< état de détection de l'interface pikawa
+    QBluetoothSocket*
+      socketBluetoothPikawa; //!< La socket cliente de communication Bluetooth
 
-    bool estBluetoothDisponible();
+    bool estBluetoothDisponible() const;
     void activerBluetooth();
-
-    bool getActive() const;
-    bool getConnecte() const;
 
   public:
     Communication(QObject* parent = nullptr);
     ~Communication();
 
+    bool estConnecte() const;
+
   public slots:
     void activerLaDecouverte();
     void decouvrirCafetiere(const QBluetoothDeviceInfo& appareilBluetooth);
+    void deconnecter();
+
+  signals:
+    void cafetiereDetectee(QString nom, QString adresse);
 };
 
 #endif // COMMUNICATION_H
