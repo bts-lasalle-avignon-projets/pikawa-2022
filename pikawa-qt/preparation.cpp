853bf8411035b414f7fcb9be5a63d7c1fe51042d
abryckaert <bryckaert.anthony@gmail.com>
2022-04-27 20:53:06 +0200

    PIKAWA22-43 Corrections des erreurs, formatage du code et ajout de la méthode LancerPreparationCafe

--- pikawa-qt/preparation.cpp
+++ pikawa-qt/preparation.cpp
@@ -104,7 +104,7 @@ void Preparation::chargerLongeurBoissons()
 
 bool Preparation::estPreparationPrete()
 {
-    if((niveauEau - cafetiere->getniveauEauNecessaire() <= 0 &&!bacPlein && tassePresente && capsulePresente)
+    if((niveauEau - cafetiere->getniveauEauNecessaire()) <= 0 &&!bacPlein && tassePresente && capsulePresente)
     {
         emit preparationPasPrete(niveauEau, bacPlein,
                 tassePresente, capsulePresente);
